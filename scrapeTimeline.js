49ef5dcacdea0422b8fcf91a33ebc22f7b7c99a3
Brad Thompson <gbradthompson@gmail.com>
2018-06-17 14:46:24 -0600

    remove debug console outs

--- scrapeTimeline.js
+++ scrapeTimeline.js
@@ -17,18 +17,17 @@ async function start () {
   let numChunks = 1000
   let chunkSize = Math.ceil(delta / numChunks)
 
+  console.log(['Date', 'Channel ID', 'Channel Name', 'Subscribers', 'Sum'].join(','))
   for (var i = 0; i < numChunks; i++) {
     let startChunk = startDate + (i * chunkSize)
     let endChunk = startChunk + chunkSize
     // console.log('chunk', chunkSize, JSON.stringify({crawlDate: {$gt: startChunk, $lt: endChunk}}))
     let largest = await db.collection('channel_info').find({crawlDate: {$gt: startChunk, $lt: endChunk}}).limit(1).sort({"info.statistics.subscriberCount": -1}).toArray()
-    let sum = await db.collection('channel_info').find({crawlDate: {$gt: startChunk, $lt: endChunk}}, {projection: {"info.statistics.subscriberCount": 1, _id: 0}}).toArray()
-    console.log(sum[0])
-    console.log(sum[0].info.statistics.subscriberCount)
-    console.log(sum.map(v => {return v.info.statistics.subscriberCount}).reduce((a, c) => { return parseInt(a) + parseInt(c) }))
     if (largest.length === 0) continue
+    let subscriberCount = await db.collection('channel_info').find({crawlDate: {$gt: startChunk, $lt: endChunk}}, {projection: {"info.statistics.subscriberCount": 1, _id: 0}}).toArray()
+    let sum = subscriberCount.map(v => {return v.info.statistics.subscriberCount}).reduce((a, c) => { return parseInt(a) + parseInt(c) })
     let date = new Date(largest[0].crawlDate)
-    console.log([date.toJSON(), largest[0].info.id, largest[0].info.snippet.title, largest[0].info.statistics.subscriberCount].join(','))
+    console.log([date.toJSON(), largest[0].info.id, largest[0].info.snippet.title, largest[0].info.statistics.subscriberCount, sum].join(','))
   }
   console.log('DONE!')
 }
