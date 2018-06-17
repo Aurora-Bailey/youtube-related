0d9aab814c722da42e6ed9f11b49363e03d3d0d0
Brad Thompson <gbradthompson@gmail.com>
2018-06-17 15:42:18 -0600

    try catch, output average and count

--- scrapeTimeline.js
+++ scrapeTimeline.js
@@ -17,18 +17,21 @@ async function start () {
   let numChunks = 1000
   let chunkSize = Math.ceil(delta / numChunks)
 
-  console.log(['Date', 'Channel ID', 'Channel Name', 'Subscribers', 'Sum'].join(','))
+  console.log(['Date', 'Channel ID', 'Channel Name', 'Subscribers', 'Sum', 'Average', 'Count'].join(','))
   for (var i = 0; i < numChunks; i++) {
-    let startChunk = startDate + (i * chunkSize)
-    let endChunk = startChunk + chunkSize
-    // console.log('chunk', chunkSize, JSON.stringify({crawlDate: {$gt: startChunk, $lt: endChunk}}))
-    let largest = await db.collection('channel_info').find({crawlDate: {$gt: startChunk, $lt: endChunk}}).limit(1).sort({"info.statistics.subscriberCount": -1}).toArray()
-    if (largest.length === 0) continue
-    let subscriberCount = await db.collection('channel_info').find({crawlDate: {$gt: startChunk, $lt: endChunk}}, {projection: {"info.statistics.subscriberCount": 1, _id: 0}}).toArray()
-    let sum = subscriberCount.map(v => {return v.info.statistics.subscriberCount}).reduce((a, c) => { return parseInt(a) + parseInt(c) })
-    let date = new Date(largest[0].crawlDate)
-    console.log([date.toJSON(), largest[0].info.id, largest[0].info.snippet.title, largest[0].info.statistics.subscriberCount, sum].join(','))
-    subscriberCount = ''
+    try {
+      let startChunk = startDate + (i * chunkSize)
+      let endChunk = startChunk + chunkSize
+      // console.log('chunk', chunkSize, JSON.stringify({crawlDate: {$gt: startChunk, $lt: endChunk}}))
+      let largest = await db.collection('channel_info').find({crawlDate: {$gt: startChunk, $lt: endChunk}}).limit(1).sort({"info.statistics.subscriberCount": -1}).toArray()
+      if (largest.length === 0) continue
+      let subscriberCount = await db.collection('channel_info').find({crawlDate: {$gt: startChunk, $lt: endChunk}}, {projection: {"info.statistics.subscriberCount": 1, _id: 0}}).toArray()
+      let sum = subscriberCount.map(v => {return v.info.statistics.subscriberCount}).reduce((a, c) => { return parseInt(a) + parseInt(c) })
+      let date = new Date(largest[0].crawlDate)
+      console.log([date.toJSON(), largest[0].info.id, largest[0].info.snippet.title, largest[0].info.statistics.subscriberCount, sum, Math.round(sum / subscriberCount.length), subscriberCount.length].join(','))
+      subscriberCount = ''
+    } catch (e) {
+    }
   }
   console.log('DONE!')
 }
